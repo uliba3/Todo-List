107b60612fd2ce8e064a5c801f0ca91eb69d9936
uliba3 <60570852+ulilba3@users.noreply.github.com>
2023-08-05 10:09:27 +0900

    change if statement checking if dueDate is null or not

--- src/dom.js
+++ src/dom.js
@@ -25,8 +25,7 @@ function renderTodos() {
 
     content.textContent = todo.content;
 
-    if (todo.dueDate === null) {
-    } else {
+    if (todo.dueDate !== null) {
       const year = todo.dueDate.getFullYear();
       const month = String(todo.dueDate.getMonth() + 1).padStart(2, '0');
       const day = String(todo.dueDate.getDate()).padStart(2, '0');
